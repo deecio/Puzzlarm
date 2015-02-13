a43404770b83184f18f766ef310546360bbe71b0
DJ Todd <dtodd67@gmail.com>
2015-02-13 10:43:54 -0600

    fixed last commit

---
+++ app/src/androidTest/java/com/project2/anything2/se329/puzzlarm/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.project2.anything2.se329.puzzlarm;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
