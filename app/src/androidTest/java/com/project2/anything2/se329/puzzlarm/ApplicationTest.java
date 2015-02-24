e200f56661a8884c30e77c610d29abcf13813f04
DJ Todd <dtodd67@gmail.com>
2015-02-24 14:38:01 -0600

    THis is all code written by Jordan harper, just some git conflics that I, as PM fixed

--- app/src/androidTest/java/com/project2/anything2/se329/puzzlarm/ApplicationTest.java
+++ app/src/androidTest/java/com/project2/anything2/se329/puzzlarm/ApplicationTest.java
@@ -1,7 +1,13 @@
 package com.project2.anything2.se329.puzzlarm;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.Intent;
+import android.provider.AlarmClock;
 import android.test.ApplicationTestCase;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +15,17 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+
+
+    public void testAlarmsSet(){
+        ArrayList<Integer> dayzzzz = new ArrayList<>();
+        dayzzzz.add(Calendar.SATURDAY);
+        Intent i = new Intent(AlarmClock.ACTION_SET_ALARM);
+        i.putExtra(AlarmClock.EXTRA_HOUR, 9);
+        i.putExtra(AlarmClock.EXTRA_MINUTES, 37);
+        i.putExtra(AlarmClock.EXTRA_DAYS, dayzzzz);
+//        startActivity(i);
     }
 }
